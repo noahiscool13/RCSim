0ea75bb4bdbbce5eda39090b2f13259179726741
Marit <maritradder@gmail.com>
2018-12-06 15:27:33 +0100

    Tom version 1. Successratio: 15/20

--- Tom.py
+++ Tom.py
@@ -1,8 +1,55 @@
-n = 1
+from math import *
+n = 0
+
 
 def ai_fun(pos, spd, dir, goal, cur):
     global n
 
-    n -= 0.0002
+    # n = 0
 
-    return [0.2, max(n, 0)]
+
+
+
+    if (dir % (2 * pi) < - 0.1 or dir % (2 * pi) > 0.1) and pos[0] < goal[cur][0]:
+        print("goal is right of me")
+        if dir % (2 * pi) > pi:
+            return[0.2,-1]
+        else:
+            return[0.2,1]
+
+    if (dir % (2 * pi) < ((1.5 * pi) - 0.1) or dir % (2 * pi) > ((1.5 * pi) + 0.1)) and pos[1] > goal[cur][1]:
+        print("its below me")
+        if dir % (2 * pi) > (1.5 * pi):
+            return [0.2, 1]
+        else:
+            return [0.2, -1]
+
+    if (dir % (2 * pi) < ((0.5 * pi) - 0.1) or dir % (2 * pi) > ((0.5 * pi) + 0.1)) and pos[1] < goal[cur][1]:
+        print("its above me")
+        if dir % (2 * pi) > (0.5 * pi):
+            return [0.2, 1]
+        else:
+            return [0.2, -1]
+
+
+    if (dir % (2 * pi) < (pi - 0.1) or dir % (2 * pi) > (pi + 0.1)) and pos[0] > goal[cur][0]:
+        print("its left of me")
+        if dir % (2 * pi) > pi:
+            return[0.2,1]
+        else:
+            return[0.2,-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
